ff43b22e12b9f684e4bc1ee9e3966128c0b28332
Colin Williams <williamsca20@gmail.com>
2020-08-26 02:24:14 -0400

    👀 Bing can make fun of Mark about the NYSM movie

---
+++ now_you_see_me.py
@@ -0,0 +1,4 @@
+import bing_bot as bb
+from datetime import date
+
+bb.send_message(f"ya know mark, today would have been day {(date.today() - date(2020, 2, 13)).days + 1} of watching Now You See Me if you weren't a quitter")
