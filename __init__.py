f27b9691c29b112b1cc3ed7187a1a026dfce671e
Colin Williams <williamsca20@gmail.com>
2021-02-27 14:02:09 -0500

    Added asking if Bing is alive and some autoformatting

--- __init__.py
+++ __init__.py
@@ -39,6 +39,10 @@ def receive_message():
     if message_contains('bing', message):
 
         # says 'hi' back to sender, and includes name if they're in H-Row
+        if message_contains('are you alive', message):
+            send_message('yeah')
+
+        # says 'hi' back to sender, and includes name if they're in H-Row
         if message_contains('hi bing', message) or message_contains('hi, bing', message):
             new_message = 'hi'
             if sender_id in SENDER_ID_TO_NAME.keys():
@@ -54,7 +58,8 @@ def receive_message():
 
         # tells a joke on demand
         if message_contains('joke', message):
-            send_message(requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'text/plain'}).text[:-1])
+            send_message(requests.get('https://icanhazdadjoke.com/',
+                                      headers={'Accept': 'text/plain'}).text[:-1])
 
         # gets weather on demand
         if message_contains('weather', message):
@@ -63,16 +68,20 @@ def receive_message():
         # gets temperature on demand
         if message_contains('temperature', message):
             temperature = get_temperature()
-            send_message(f'{temperature}{" (nice)" if temperature == "69" else ""}')
+            send_message(
+                f'{temperature}{" (nice)" if temperature == "69" else ""}')
 
         # make a new meme on demand
         if message_contains('make', message) and message_contains('meme', message):
-            send_meme(f'''ok {SENDER_ID_TO_NAME[data["sender_id"]]}, here's a new meme''')
+            send_meme(
+                f'''ok {SENDER_ID_TO_NAME[data["sender_id"]]}, here's a new meme''')
 
         # gives a random recipe
         if message_contains('cook', message) or message_contains('meal', message) or message_contains('dinner', message) or message_contains('lunch', message):
-            recipe = requests.get('https://www.themealdb.com/api/json/v1/1/random.php').json()
-            send_message(f'you should have {recipe["meals"][0]["strMeal"].lower()}' + (("\n\n" + recipe['meals'][0]['strYoutube']) if recipe["meals"][0]["strYoutube"] else "") + (("\n\n" + recipe['meals'][0]['strSource']) if recipe["meals"][0]["strSource"] else ""))
+            recipe = requests.get(
+                'https://www.themealdb.com/api/json/v1/1/random.php').json()
+            send_message(f'you should have {recipe["meals"][0]["strMeal"].lower()}' + (("\n\n" + recipe['meals'][0]['strYoutube']) if recipe["meals"]
+                                                                                       [0]["strYoutube"] else "") + (("\n\n" + recipe['meals'][0]['strSource']) if recipe["meals"][0]["strSource"] else ""))
 
     # has something for the good of the order
     if message_contains('good of the order', message):
