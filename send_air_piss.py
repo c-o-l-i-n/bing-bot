5b685e8356fb2ff1fe9a98a32f67fce858d4d662
Colin Williams <williamsca20@gmail.com>
2021-07-08 17:10:51 -0400

    💨 Sends a message if the humidity is at least 90

---
+++ send_air_piss.py
@@ -0,0 +1,12 @@
+from send_message import send_message
+from weather import get_humidity
+import fix_path
+
+def air_piss():
+    humidity = get_humidity()
+
+    if humidity >= 90:
+        send_message('the air is full of piss')
+
+if __name__ == '__main__':
+    air_piss()
--- weather.py
+++ weather.py
@@ -1,9 +1,17 @@
+from secrets import WEATHER_API_KEY
 import requests
 
+def _get_weather_api():
+    return requests.get(f'http://api.openweathermap.org/data/2.5/weather?id=4509177&appid={WEATHER_API_KEY}')
+
 def get_weather():
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather?id=4509177&appid=48036d8b91dc62762efc28095e51b57d')
+    response = _get_weather_api()
     return response.json()['weather'][0]['main'].lower()
 
 def get_temperature():
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather?id=4509177&appid=48036d8b91dc62762efc28095e51b57d')
+    response = _get_weather_api()
     return f"{(response.json()['main']['temp'] * (9 / 5) - 459.67):.0f} °F"
+
+def get_humidity():
+    response = _get_weather_api()
+    return response.json()['main']['humidity']
