9ca825ace25d731fc01d433b4d8c79ef96242b91
Colin Williams <williamsca20@gmail.com>
2022-02-20 17:53:49 -0500

    🗃 Settings database works!

--- src/app.py
+++ src/app.py
@@ -43,40 +43,40 @@ def receive_message():
     if message_contains('bing', message):
 
         # says 'hi' back to sender, and includes name if they're in H-Row
-        if settings['are you alive'] and message_contains('are you alive', message):
+        if setting_is_turned_on('are you alive', settings) and message_contains('are you alive', message):
             send_message('yeah')
 
         # says 'hi' back to sender, and includes name if they're in H-Row
-        if settings['hi bing'] and message_contains('hi bing', message) or message_contains('hi, bing', message):
+        if setting_is_turned_on('hi bing', settings) and message_contains('hi bing', message) or message_contains('hi, bing', message):
             new_message = 'hi'
             if sender_id in GROUPME_USER_ID_TO_NAME.keys():
                 new_message += f' {GROUPME_USER_ID_TO_NAME[sender_id]}'
             send_message(new_message)
 
         # says 'i love you' back to sender, and includes name if they're in H-Row
-        if settings['i love you'] and message_contains('i love you', message):
+        if setting_is_turned_on('i love you', settings) and message_contains('i love you', message):
             new_message = 'i love you too'
             if sender_id in GROUPME_USER_ID_TO_NAME.keys():
                 new_message += f' {GROUPME_USER_ID_TO_NAME[sender_id]}'
             send_message(new_message)
 
         # tells a joke on demand
-        if settings['joke'] and message_contains('joke', message):
+        if setting_is_turned_on('joke', settings) and message_contains('joke', message):
             send_message(requests.get('https://icanhazdadjoke.com/',
                                       headers={'Accept': 'text/plain'}).text[:-1].lower())
 
         # gets weather on demand
-        if settings['weather'] and message_contains('weather', message):
+        if setting_is_turned_on('weather', settings) and message_contains('weather', message):
             send_message(get_weather())
 
         # gets temperature on demand
-        if settings['temperature'] and message_contains('temperature', message):
+        if setting_is_turned_on('temperature', settings) and message_contains('temperature', message):
             temperature = get_temperature()
             send_message(
                 f'{temperature}{" (nice)" if "69" in temperature else ""}')
 
         # make a new meme on demand
-        if settings['make meme'] and (message_contains('make', message) or message_contains('send', message)) and message_contains('meme', message):
+        if setting_is_turned_on('make meme', settings) and (message_contains('make', message) or message_contains('send', message)) and message_contains('meme', message):
             if sender_id in GROUPME_USER_ID_TO_NAME.keys():
                 message_text = f'''ok {GROUPME_USER_ID_TO_NAME[data["sender_id"]]}, here's a new meme'''
             else:
@@ -87,50 +87,50 @@ def receive_message():
                 send_meme(message_text=message_text)
 
         # gives a random recipe
-        if settings['cook meal'] and message_contains('cook', message) or message_contains('meal', message) or message_contains('dinner', message) or message_contains('lunch', message):
+        if setting_is_turned_on('cook meal', settings) and message_contains('cook', message) or message_contains('meal', message) or message_contains('dinner', message) or message_contains('lunch', message):
             recipe = requests.get(
                 'https://www.themealdb.com/api/json/v1/1/random.php').json()
             send_message(f'you should have {recipe["meals"][0]["strMeal"].lower()}' + (("\n\n" + recipe['meals'][0]['strYoutube']) if recipe["meals"]
                                                                                        [0]["strYoutube"] else "") + (("\n\n" + recipe['meals'][0]['strSource']) if recipe["meals"][0]["strSource"] else ""))
 
         # sends a quote from the movie "The Car"
-        if settings['car quote'] and message_contains('car', message) and (message_contains('line', message) or message_contains('quote', message)):
+        if setting_is_turned_on('car quote', settings) and message_contains('car', message) and (message_contains('line', message) or message_contains('quote', message)):
             send_the_car_quote()
 
     # has something for the good of the order
-    if settings['good of the order'] and message_contains('good of the order', message):
+    if setting_is_turned_on('good of the order', settings) and message_contains('good of the order', message):
         send_message('tits')
 
     # sings "One Pizza Pie"
-    if settings['one pizza pie'] and 'one pizza pie' == message[-13:].lower() or 'one pizza pie' == message[-14:-1].lower() or '1 pizza pie' == message[-11:].lower() or '1 for me' == message[-12:-1].lower():
+    if setting_is_turned_on('one pizza pie', settings) and 'one pizza pie' == message[-13:].lower() or 'one pizza pie' == message[-14:-1].lower() or '1 pizza pie' == message[-11:].lower() or '1 for me' == message[-12:-1].lower():
         send_message('🍕 one for me, 🍕 one for when i die')
 
     # sings "One Pizza Pie"
-    if settings['one pizza pie'] and 'one for me' == message[-10:].lower() or 'one for me' == message[-11:-1].lower() or '1 for me' == message[-8:].lower() or '1 for me' == message[-9:-1].lower():
+    if setting_is_turned_on('one pizza pie', settings) and 'one for me' == message[-10:].lower() or 'one for me' == message[-11:-1].lower() or '1 for me' == message[-8:].lower() or '1 for me' == message[-9:-1].lower():
         send_message('🍕 one for when i die')
 
     # says "nice" when someone else says 69 or 420
-    if settings['69 420'] and data['sender_type'] != 'bot' and (message_contains('69', message) or message_contains('420', message)):
+    if setting_is_turned_on('69 420', settings) and data['sender_type'] != 'bot' and (message_contains('69', message) or message_contains('420', message)):
         send_message('nice')
 
     # says "ass" after "h"
-    if settings['h ass ohio you suck'] and data['sender_type'] != 'bot' and 'h' == message.lower():
+    if setting_is_turned_on('h ass ohio you suck', settings) and data['sender_type'] != 'bot' and 'h' == message.lower():
         send_message('ass')
 
     # says "ohio" after "ass"
-    if settings['are you alive'] and data['sender_type'] != 'bot' and 'ass' == message.lower():
+    if setting_is_turned_on('are you alive', settings) and data['sender_type'] != 'bot' and 'ass' == message.lower():
         send_message('ohio')
 
     # says "you suck" after "ohio"
-    if settings['h ass ohio you suck'] and data['sender_type'] != 'bot' and 'ohio' == message.lower():
+    if setting_is_turned_on('h ass ohio you suck', settings) and data['sender_type'] != 'bot' and 'ohio' == message.lower():
         send_message('you suck!')
 
     # says "ohio, you suck" after "h, ass"
-    if settings['h ass ohio you suck'] and data['sender_type'] != 'bot' and ('h ass' == message[-5:].lower() or 'h, ass' == message[-6:].lower()):
+    if setting_is_turned_on('h ass ohio you suck', settings) and data['sender_type'] != 'bot' and ('h ass' == message[-5:].lower() or 'h, ass' == message[-6:].lower()):
         send_message('ohio, you suck!')
 
     # says cat call if message sent by a woman in H-Row
-    if settings['cat call'] and sender_id in WOMEN_GROUPME_USER_IDS:
+    if setting_is_turned_on('cat call', settings) and sender_id in WOMEN_GROUPME_USER_IDS:
         send_message(f'@{GROUPME_USER_ID_TO_NAME[sender_id]}', 'https://i.groupme.com/256x274.jpeg.ffbbd45a599d4756911bd92442a39440')
 
     return "ok", 200
@@ -146,16 +146,17 @@ def get():
 
 
 def get_settings():
-    settings = {}
-    for setting in Setting.query.all():
-        settings[setting.name] = setting.value
-    return settings
+    return Setting.query.all()
 
 
 def set_settings(new_settings):
     for setting in Setting.query.all():
         setting.value = setting.name in new_settings
     db.session.commit()
+
+
+def setting_is_turned_on(setting, settings):
+    return next(filter(lambda x: x.name == setting, settings), None).value
 
 
 @app.route('/settings', methods=['GET', 'POST'])
@@ -171,8 +172,18 @@ def settings():
             new_settings.remove('submission')
             new_settings = [x.replace('-', ' ') for x in new_settings]
             set_settings(new_settings)
-            return render_template('settings.html', settings=get_settings(), password=BING_SETTINGS_PASSWORD, settings_saved=True)
-        return render_template('settings.html', settings=get_settings(), password=BING_SETTINGS_PASSWORD)
+
+            # reload with new settings
+            settings = get_settings()
+            command_settings = [setting for setting in sorted(settings, key=lambda x: x.category_position) if setting.category == 'command']
+            scheduled_settings = [setting for setting in sorted(settings, key=lambda x: x.category_position) if setting.category == 'scheduled']
+            return render_template('settings.html', command_settings=command_settings, scheduled_settings=scheduled_settings, password=BING_SETTINGS_PASSWORD, settings_saved=True)
+        
+        # load settings
+        settings = get_settings()
+        command_settings = [setting for setting in sorted(settings, key=lambda x: x.category_position) if setting.category == 'command']
+        scheduled_settings = [setting for setting in sorted(settings, key=lambda x: x.category_position) if setting.category == 'scheduled']
+        return render_template('settings.html', command_settings=command_settings, scheduled_settings=scheduled_settings, password=BING_SETTINGS_PASSWORD)
 
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -8,11 +8,15 @@ class Setting(db.Model):
     __tablename__ = 'settings'
 
     name = db.Column(db.String(), primary_key=True)
-    value = db.Column(db.Boolean, nullable=False, default=True)
+    value = db.Column(db.Boolean, nullable=False, server_default='True')
+    category = db.Column(db.String(), nullable=False, server_default='command')
+    category_position = db.Column(db.Integer, nullable=False, server_default='0')
 
-    def __init__(self, name, value):
+    def __init__(self, name, value, category, category_position):
         self.name = name
         self.value = value
+        self.category = category
+        self.category_position = category_position
 
     def __repr__(self):
         return f'<Setting {self.name}>'
@@ -23,7 +27,7 @@ class GroupmeUser(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String, nullable=False)
-    is_woman = db.Column(db.Boolean, nullable=False, default=False)
+    is_woman = db.Column(db.Boolean, nullable=False, server_default='False')
 
     def __init__(self, nickname, is_woman):
         self.nickname = nickname
