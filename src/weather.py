6241e18bb27de6392d18d3b2e5dd11269de06b61
Colin Williams <williamsca20@gmail.com>
2022-02-21 22:06:11 -0500

    🪵 Added logging to weather.py

--- src/weather.py
+++ src/weather.py
@@ -1,24 +1,42 @@
 import os
+import logging
 import requests
 
 
 WEATHER_API_KEY = os.environ['WEATHER_API_KEY']
 
 
-def _get_weather_api():
-    return requests.get(f'http://api.openweathermap.org/data/2.5/weather?id=4509177&appid={WEATHER_API_KEY}')
+def _get_weather_data():
+    logging.info('Getting current weather data for Columbus')
+    response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?id=4509177&appid={WEATHER_API_KEY}')
+
+    if response.status_code != 200:
+        logging.error(response.text)
+        response.raise_for_status() 
+
+    logging.info(response)
+    return response
 
 
 def get_weather():
-    response = _get_weather_api()
-    return response.json()['weather'][0]['main'].lower()
+    try:
+        response = _get_weather_data()
+        return response.json()['weather'][0]['main'].lower()
+    except:
+        return ''
 
 
 def get_temperature():
-    response = _get_weather_api()
-    return f"{(response.json()['main']['temp'] * (9 / 5) - 459.67):.0f} °F"
+    try:
+        response = _get_weather_data()
+        return f"{(response.json()['main']['temp'] * (9 / 5) - 459.67):.0f} °F"
+    except:
+        return ''
 
 
 def get_humidity():
-    response = _get_weather_api()
-    return response.json()['main']['humidity']
+    try:
+        response = _get_weather_data()
+        return response.json()['main']['humidity']
+    except:
+        return ''
