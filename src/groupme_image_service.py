3f92e96de193fb4d9127293d633b40fa2bc4e840
Colin Williams <williamsca20@gmail.com>
2022-02-21 21:52:52 -0500

    🪵 Added logging to groupme_image_service.py

--- src/groupme_image_service.py
+++ src/groupme_image_service.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import requests
 from io import BytesIO
 
@@ -7,14 +8,24 @@ GROUPME_ACCESS_TOKEN = os.environ['GROUPME_ACCESS_TOKEN']
 
 
 def upload_image_data(image_data):
+    logging.info('Uploading image data to GroupMe image service')
+
     response = requests.post(url='https://image.groupme.com/pictures',
                         data=image_data,
                         headers={'Content-Type': 'image/jpeg',
                                 'X-Access-Token': GROUPME_ACCESS_TOKEN}).json()
+
+    if response.status_code != 200:
+        logging.error(response)
+        return ''
+
     groupme_image_url = response['payload']['picture_url']
+    logging.info(f'Image uploaded to {groupme_image_url}')
+
     return groupme_image_url
 
 
 def upload_image_url(url):
+    logging.info(f'Uploading image URL to GroupMe image service: {url}')
     image_data = BytesIO(requests.get(url).content)
     return upload_image_data(image_data)
